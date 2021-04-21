4e4e4d111a524fe2081795890898f4f54d3e8521
thech <jairo@entornos.es>
2021-04-21 10:07:12 +0200

    Creación de bucles.

---
+++ HolaMundoGit/src/HolaMundo.java
@@ -0,0 +1,18 @@
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		System.out.println("Hola mundo.");
+		
+		int n1 = 2;
+		int n2 = 4;
+		
+		if (n1 < n2) {
+			System.out.println("N1 es menor.");
+		}
+		else {
+			System.out.println("N2 es menor.");
+		}
+	}
+
+}
